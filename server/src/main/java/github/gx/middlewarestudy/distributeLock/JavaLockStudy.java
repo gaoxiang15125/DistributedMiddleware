3a066ff20e1680f210c275e3def3814479ffc77e
gaoxiang <630268696@qq.com>
2021-02-28 20:19:04 +0800

    Java 相关锁机制
    具体相关的东东，读读 《Java 并发编程的艺术》这本书吧

---
+++ server/src/main/java/github/gx/middlewarestudy/distributeLock/JavaLockStudy.java
@@ -0,0 +1,18 @@
+package github.gx.middlewarestudy.distributeLock;
+
+/**
+ * @program: MiddlewareStudy
+ * @description: Java 本地锁实例 使用 Java 并发相关 关键字实现锁操作
+ * @author: gaoxiang
+ * @email: 630268696@qq.com
+ * @create: 2021-02-28 17:25
+ **/
+public class JavaLockStudy {
+    // 相对而言比较简单 不再进行 编写
+    // 值得一提的是 synchronized 采用的实现原理 值得关于
+    // 可以尝试使用 volatile 实现 synchronized 关键字
+    // Lock ？ TODO 相关知识可以学习以下
+//    Lock
+    // 问题也很明显 这些都是针对 单应用程序的 单体锁，不适合分布式场景下使用
+
+}
